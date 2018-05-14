6df7486c4611d641a845fbc4211a4816f9b9189c
kagtag <798066354@qq.com>
2018-05-14 23:10:21 +0800

    Blend Demo can run

---
+++ BlendDemo/BlendDemo.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include "d3dApp.h"
+#include "d3dx11effect.h"
+#include "GeometryGenerator.h"
+#include "MathHelper.h"
+#include "LightHelper.h"
+#include "Effects.h"
+#include "Vertex.h"
+#include "RenderStates.h"
+#include "Waves.h"
+
+enum RenderOptions
+{
+	Lighting = 0,
+	Textures = 1,
+	TexturesAndFog = 2,
+};
+
+class BlendApp : public D3DApp
+{
+public:
+	BlendApp(HINSTANCE hInstance);
+	~BlendApp();
+
+	bool Init();
+	void OnResize();
+	void UpdateScene(float dt);
+	bool DrawScene();
+
+	void OnMouseDown(WPARAM btnState, int x, int y);
+	void OnMouseUp(WPARAM btnState, int x, int y);
+	void OnMouseMove(WPARAM btnState, int x, int y);
+
+private:
+	float GetHillHeight(float x, float z)const;
+	XMFLOAT3 GetHillNormal(float x, float z)const;
+	void BuildLandGeometryBuffers();
+	void BuildWaveGeometryBuffers();
+	void BuildCrateGeometryBuffers();
+
+private:
+	ID3D11Buffer* mLandVB;
+	ID3D11Buffer* mLandIB;
+
+	ID3D11Buffer* mWavesVB;
+	ID3D11Buffer* mWavesIB;
+
+	ID3D11Buffer* mBoxVB;
+	ID3D11Buffer* mBoxIB;
+
+	ID3D11ShaderResourceView* mGrassMapSRV;
+	ID3D11ShaderResourceView* mWavesMapSRV;
+	ID3D11ShaderResourceView* mBoxMapSRV;
+
+	Waves mWaves;
+
+	DirectionalLight mDirLights[3];
+	Material mLandMat;
+	Material mWavesMat;
+	Material mBoxMat;
+
+	XMFLOAT4X4 mGrassTexTransform;
+	XMFLOAT4X4 mWaterTexTransform;
+
+	XMFLOAT4X4 mLandWorld;
+	XMFLOAT4X4 mWavesWorld;
+	XMFLOAT4X4 mBoxWorld;
+
+	XMFLOAT4X4 mView;
+	XMFLOAT4X4 mProj;
+
+	UINT mLandIndexCount;
+
+	XMFLOAT2 mWaterTexOffset;
+
+	RenderOptions mRenderOptions;
+
+	XMFLOAT3 mEyePosW;
+
+	float mTheta;
+	float mPhi;
+	float mRadius;
+
+	POINT mLastMousePos;
+
+};
+
