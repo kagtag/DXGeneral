2845fd51648138fd0b13ce2331c3a901873755e9
kagtag <798066354@qq.com>
2019-02-16 20:03:54 +0800

    Tessellation, Bezier patch can run

---
+++ BezierPatch/Vertex.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "d3dUtil.h"
+
+namespace Vertex
+{
+	struct Pos
+	{
+		float x;
+		float y;
+		float z;
+	};
+
+	struct Basic32
+	{
+		XMFLOAT3 Pos;
+		XMFLOAT3 Normal;
+		XMFLOAT2 Tex;
+	};
+}
+
+class InputLayoutDesc
+{
+public:
+	static const D3D11_INPUT_ELEMENT_DESC Pos[1];
+	static const D3D11_INPUT_ELEMENT_DESC Basic32[3];
+};
+
+class InputLayouts
+{
+public:
+	static void InitAll(ID3D11Device* device);
+	static void DestroyAll();
+
+	static ID3D11InputLayout* Pos;
+	static ID3D11InputLayout* Basic32;
+};
