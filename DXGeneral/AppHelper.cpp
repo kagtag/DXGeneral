24a60e91d29c97147aef482c8923c7622b64946a
kagtag <798066354@qq.com>
2019-02-22 00:32:57 +0800

    Refactoring CubeMapApp

---
+++ DXGeneral/AppHelper.cpp
@@ -0,0 +1,352 @@
+#include "AppHelper.h"
+
+#pragma region CommonApp
+CommonApp::CommonApp(HINSTANCE hInstance)
+	:D3DApp(hInstance)
+{
+	mLastMousePos.x = 0;
+	mLastMousePos.y = 0;
+
+	mCam.SetPosition(0.0f, 2.0f, -15.0f);
+}
+
+void CommonApp::OnResize()
+{
+	D3DApp::OnResize();
+
+	mCam.SetLens(0.25f*MathHelper::Pi, AspectRatio(), 1.0f, 1000.0f);
+}
+
+void CommonApp::UpdateScene(float dt)
+{
+	//
+	// Control the camera.
+	//
+	if (GetAsyncKeyState('W') & 0x8000)
+		mCam.Walk(10.0f*dt);
+
+	if (GetAsyncKeyState('S') & 0x8000)
+		mCam.Walk(-10.0f*dt);
+
+	if (GetAsyncKeyState('A') & 0x8000)
+		mCam.Strafe(-10.0f*dt);
+
+	if (GetAsyncKeyState('D') & 0x8000)
+		mCam.Strafe(10.0f*dt);
+
+
+	mCam.UpdateViewMatrix();
+
+}
+
+
+void CommonApp::OnMouseDown(WPARAM btnState, int x, int y)
+{
+	mLastMousePos.x = x;
+	mLastMousePos.y = y;
+
+	SetCapture(m_hMainWnd);
+}
+
+void CommonApp::OnMouseUp(WPARAM btnState, int x, int y)
+{
+	ReleaseCapture();
+}
+
+void CommonApp::OnMouseMove(WPARAM btnState, int x, int y)
+{
+	if ((btnState & MK_LBUTTON) != 0)
+	{
+		// Make each pixel correspond to a quarter of a degree.
+		float dx = XMConvertToRadians(0.25f*static_cast<float>(x - mLastMousePos.x));
+		float dy = XMConvertToRadians(0.25f*static_cast<float>(y - mLastMousePos.y));
+
+		mCam.Pitch(dy);
+		mCam.RotateY(dx);
+	}
+
+	mLastMousePos.x = x;
+	mLastMousePos.y = y;
+}
+
+#pragma endregion
+
+
+#include "Vertex.h"
+
+#pragma region ShapesBaseApp
+
+ShapesBaseApp::ShapesBaseApp(HINSTANCE hInstance)
+	:CommonApp(hInstance),
+	mShapesVB(0), mShapesIB(0),
+	mSkullVB(0), mSkullIB(0),
+	mFloorTexSRV(0), mStoneTexSRV(0), mBrickTexSRV(0),
+	mSkullIndexCount(0),
+	
+	mLightCount(3)
+{
+
+	XMMATRIX I = XMMatrixIdentity();
+	XMStoreFloat4x4(&mGridWorld, I);
+
+	XMMATRIX boxScale = XMMatrixScaling(3.0f, 1.0f, 3.0f);
+	XMMATRIX boxOffset = XMMatrixTranslation(0.0f, 0.5f, 0.0f);
+	XMStoreFloat4x4(&mBoxWorld, XMMatrixMultiply(boxScale, boxOffset));
+
+
+	for (int i = 0; i < 5; ++i)
+	{
+		XMStoreFloat4x4(&mCylWorld[i * 2 + 0], XMMatrixTranslation(-5.0f, 1.5f, -10.0f + i*5.0f));
+		XMStoreFloat4x4(&mCylWorld[i * 2 + 1], XMMatrixTranslation(+5.0f, 1.5f, -10.0f + i*5.0f));
+
+		XMStoreFloat4x4(&mSphereWorld[i * 2 + 0], XMMatrixTranslation(-5.0f, 3.5f, -10.0f + i*5.0f));
+		XMStoreFloat4x4(&mSphereWorld[i * 2 + 1], XMMatrixTranslation(+5.0f, 3.5f, -10.0f + i*5.0f));
+	}
+
+	mDirLights[0].Ambient = XMFLOAT4(0.2f, 0.2f, 0.2f, 1.0f);
+	mDirLights[0].Diffuse = XMFLOAT4(0.5f, 0.5f, 0.5f, 1.0f);
+	mDirLights[0].Specular = XMFLOAT4(0.5f, 0.5f, 0.5f, 1.0f);
+	mDirLights[0].Direction = XMFLOAT3(0.57735f, -0.57735f, 0.57735f);
+
+	mDirLights[1].Ambient = XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f);
+	mDirLights[1].Diffuse = XMFLOAT4(0.20f, 0.20f, 0.20f, 1.0f);
+	mDirLights[1].Specular = XMFLOAT4(0.25f, 0.25f, 0.25f, 1.0f);
+	mDirLights[1].Direction = XMFLOAT3(-0.57735f, -0.57735f, 0.57735f);
+
+	mDirLights[2].Ambient = XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f);
+	mDirLights[2].Diffuse = XMFLOAT4(0.2f, 0.2f, 0.2f, 1.0f);
+	mDirLights[2].Specular = XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f);
+	mDirLights[2].Direction = XMFLOAT3(0.0f, -0.707f, -0.707f);
+
+	mGridMat.Ambient = XMFLOAT4(0.8f, 0.8f, 0.8f, 1.0f);
+	mGridMat.Diffuse = XMFLOAT4(0.8f, 0.8f, 0.8f, 1.0f);
+	mGridMat.Specular = XMFLOAT4(0.8f, 0.8f, 0.8f, 16.0f);
+	mGridMat.Reflect = XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f);
+
+	mCylinderMat.Ambient = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+	mCylinderMat.Diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+	mCylinderMat.Specular = XMFLOAT4(0.8f, 0.8f, 0.8f, 16.0f);
+	mCylinderMat.Reflect = XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f);
+
+	mSphereMat.Ambient = XMFLOAT4(0.2f, 0.3f, 0.4f, 1.0f);
+	mSphereMat.Diffuse = XMFLOAT4(0.2f, 0.3f, 0.4f, 1.0f);
+	mSphereMat.Specular = XMFLOAT4(0.9f, 0.9f, 0.9f, 16.0f);
+	mSphereMat.Reflect = XMFLOAT4(0.4f, 0.4f, 0.4f, 1.0f);
+
+	mBoxMat.Ambient = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+	mBoxMat.Diffuse = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+	mBoxMat.Specular = XMFLOAT4(0.8f, 0.8f, 0.8f, 16.0f);
+	mBoxMat.Reflect = XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f);
+
+	mSkullMat.Ambient = XMFLOAT4(0.2f, 0.2f, 0.2f, 1.0f);
+	mSkullMat.Diffuse = XMFLOAT4(0.2f, 0.2f, 0.2f, 1.0f);
+	mSkullMat.Specular = XMFLOAT4(0.8f, 0.8f, 0.8f, 16.0f);
+	mSkullMat.Reflect = XMFLOAT4(0.5f, 0.5f, 0.5f, 1.0f);
+
+}
+
+ShapesBaseApp::~ShapesBaseApp()
+{
+	ReleaseCOM(mShapesVB);
+	ReleaseCOM(mShapesIB);
+
+	ReleaseCOM(mSkullVB);
+	ReleaseCOM(mSkullIB);
+	
+	ReleaseCOM(mFloorTexSRV);
+	ReleaseCOM(mStoneTexSRV);
+	ReleaseCOM(mBrickTexSRV);
+
+}
+
+bool ShapesBaseApp::Init()
+{
+	if (!D3DApp::Init())
+		return false;
+
+	HR(CreateDDSTextureFromFile(m_d3dDevice, L"../Textures/floor.dds", 0, &mFloorTexSRV));
+
+	HR(CreateDDSTextureFromFile(m_d3dDevice, L"../Textures/stone.dds", 0, &mStoneTexSRV));
+
+	HR(CreateDDSTextureFromFile(m_d3dDevice, L"../Textures/bricks.dds", 0, &mBrickTexSRV));
+
+	BuildShapeGeometryBuffers();
+	BuildSkullGeometryBuffers();
+
+	return true;
+}
+
+void ShapesBaseApp::BuildShapeGeometryBuffers()
+{
+	GeometryGenerator::MeshData box;
+	GeometryGenerator::MeshData grid;
+	GeometryGenerator::MeshData sphere;
+	GeometryGenerator::MeshData cylinder;
+
+	GeometryGenerator geoGen;
+	geoGen.CreateBox(1.0f, 1.0f, 1.0f, box);
+	geoGen.CreateGrid(20.0f, 30.0f, 60, 40, grid);
+	geoGen.CreateSphere(0.5f, 20, 20, sphere);
+	geoGen.CreateCylinder(0.5f, 0.3f, 3.0f, 20, 20, cylinder);
+
+	// Cache the vertex offsets to each object in the concatenated vertex buffer.
+	mBoxVertexOffset = 0;
+	mGridVertexOffset = box.Vertices.size();
+	mSphereVertexOffset = mGridVertexOffset + grid.Vertices.size();
+	mCylinderVertexOffset = mSphereVertexOffset + sphere.Vertices.size();
+
+	// Cache the index count of each object.
+	mBoxIndexCount = box.Indices.size();
+	mGridIndexCount = grid.Indices.size();
+	mSphereIndexCount = sphere.Indices.size();
+	mCylinderIndexCount = cylinder.Indices.size();
+
+	// Cache the starting index for each object in the concatenated index buffer.
+	mBoxIndexOffset = 0;
+	mGridIndexOffset = mBoxIndexCount;
+	mSphereIndexOffset = mGridIndexOffset + mGridIndexCount;
+	mCylinderIndexOffset = mSphereIndexOffset + mSphereIndexCount;
+
+	UINT totalVertexCount =
+		box.Vertices.size() +
+		grid.Vertices.size() +
+		sphere.Vertices.size() +
+		cylinder.Vertices.size();
+
+	UINT totalIndexCount =
+		mBoxIndexCount +
+		mGridIndexCount +
+		mSphereIndexCount +
+		mCylinderIndexCount;
+
+	//
+	// Extract the vertex elements we are interested in and pack the
+	// vertices of all the meshes into one vertex buffer.
+	//
+
+	std::vector<Vertex::Basic32> vertices(totalVertexCount);
+
+	UINT k = 0;
+	for (size_t i = 0; i < box.Vertices.size(); ++i, ++k)
+	{
+		vertices[k].Pos = box.Vertices[i].Position;
+		vertices[k].Normal = box.Vertices[i].Normal;
+		vertices[k].Tex = box.Vertices[i].TexC;
+	}
+
+	for (size_t i = 0; i < grid.Vertices.size(); ++i, ++k)
+	{
+		vertices[k].Pos = grid.Vertices[i].Position;
+		vertices[k].Normal = grid.Vertices[i].Normal;
+		vertices[k].Tex = grid.Vertices[i].TexC;
+	}
+
+	for (size_t i = 0; i < sphere.Vertices.size(); ++i, ++k)
+	{
+		vertices[k].Pos = sphere.Vertices[i].Position;
+		vertices[k].Normal = sphere.Vertices[i].Normal;
+		vertices[k].Tex = sphere.Vertices[i].TexC;
+	}
+
+	for (size_t i = 0; i < cylinder.Vertices.size(); ++i, ++k)
+	{
+		vertices[k].Pos = cylinder.Vertices[i].Position;
+		vertices[k].Normal = cylinder.Vertices[i].Normal;
+		vertices[k].Tex = cylinder.Vertices[i].TexC;
+	}
+
+	D3D11_BUFFER_DESC vbd;
+	vbd.Usage = D3D11_USAGE_IMMUTABLE;
+	vbd.ByteWidth = sizeof(Vertex::Basic32) * totalVertexCount;
+	vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vbd.CPUAccessFlags = 0;
+	vbd.MiscFlags = 0;
+	D3D11_SUBRESOURCE_DATA vinitData;
+	vinitData.pSysMem = &vertices[0];
+	HR(m_d3dDevice->CreateBuffer(&vbd, &vinitData, &mShapesVB));
+
+	//
+	// Pack the indices of all the meshes into one index buffer.
+	//
+
+	std::vector<UINT> indices;
+	indices.insert(indices.end(), box.Indices.begin(), box.Indices.end());
+	indices.insert(indices.end(), grid.Indices.begin(), grid.Indices.end());
+	indices.insert(indices.end(), sphere.Indices.begin(), sphere.Indices.end());
+	indices.insert(indices.end(), cylinder.Indices.begin(), cylinder.Indices.end());
+
+	D3D11_BUFFER_DESC ibd;
+	ibd.Usage = D3D11_USAGE_IMMUTABLE;
+	ibd.ByteWidth = sizeof(UINT) * totalIndexCount;
+	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	ibd.CPUAccessFlags = 0;
+	ibd.MiscFlags = 0;
+	D3D11_SUBRESOURCE_DATA iinitData;
+	iinitData.pSysMem = &indices[0];
+	HR(m_d3dDevice->CreateBuffer(&ibd, &iinitData, &mShapesIB));
+}
+
+void ShapesBaseApp::BuildSkullGeometryBuffers()
+{
+	std::ifstream fin("../Models/skull.txt");
+
+	if (!fin)
+	{
+		MessageBox(0, L"../Models/skull.txt not found.", 0, 0);
+		return;
+	}
+
+	UINT vcount = 0;
+	UINT tcount = 0;
+	std::string ignore;
+
+	fin >> ignore >> vcount;
+	fin >> ignore >> tcount;
+	fin >> ignore >> ignore >> ignore >> ignore;
+
+	std::vector<Vertex::Basic32> vertices(vcount);
+	for (UINT i = 0; i < vcount; ++i)
+	{
+		fin >> vertices[i].Pos.x >> vertices[i].Pos.y >> vertices[i].Pos.z;
+		fin >> vertices[i].Normal.x >> vertices[i].Normal.y >> vertices[i].Normal.z;
+	}
+
+	fin >> ignore;
+	fin >> ignore;
+	fin >> ignore;
+
+	mSkullIndexCount = 3 * tcount;
+	std::vector<UINT> indices(mSkullIndexCount);
+	for (UINT i = 0; i < tcount; ++i)
+	{
+		fin >> indices[i * 3 + 0] >> indices[i * 3 + 1] >> indices[i * 3 + 2];
+	}
+
+	fin.close();
+
+	D3D11_BUFFER_DESC vbd;
+	vbd.Usage = D3D11_USAGE_IMMUTABLE;
+	vbd.ByteWidth = sizeof(Vertex::Basic32) * vcount;
+	vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vbd.CPUAccessFlags = 0;
+	vbd.MiscFlags = 0;
+	D3D11_SUBRESOURCE_DATA vinitData;
+	vinitData.pSysMem = &vertices[0];
+	HR(m_d3dDevice->CreateBuffer(&vbd, &vinitData, &mSkullVB));
+
+	//
+	// Pack the indices of all the meshes into one index buffer.
+	//
+
+	D3D11_BUFFER_DESC ibd;
+	ibd.Usage = D3D11_USAGE_IMMUTABLE;
+	ibd.ByteWidth = sizeof(UINT) * mSkullIndexCount;
+	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	ibd.CPUAccessFlags = 0;
+	ibd.MiscFlags = 0;
+	D3D11_SUBRESOURCE_DATA iinitData;
+	iinitData.pSysMem = &indices[0];
+	HR(m_d3dDevice->CreateBuffer(&ibd, &iinitData, &mSkullIB));
+}
+
+#pragma endregion
--- DXGeneral/AppHelper.h
+++ DXGeneral/AppHelper.h
@@ -33,3 +33,91 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, \
 }
 
 #endif 
+
+// Common methods appear in almost all Apps
+// mainly First Person Camera operation
+class CommonApp : public D3DApp
+{
+public:
+	CommonApp(HINSTANCE hInstance);
+
+	void OnResize();
+
+	void UpdateScene(float dt);
+
+	void OnMouseDown(WPARAM btnState, int x, int y);
+	void OnMouseUp(WPARAM btnState, int x, int y);
+	void OnMouseMove(WPARAM btnState, int x, int y);
+
+protected:
+
+	Camera mCam;
+
+	POINT mLastMousePos;
+};
+
+
+// Shapes scene in the book
+// Geometries, textures, materials and lights
+class ShapesBaseApp : public CommonApp
+{
+public:
+	ShapesBaseApp(HINSTANCE hInstance);
+	~ShapesBaseApp();
+
+	bool Init();
+
+protected:
+	void BuildShapeGeometryBuffers();
+	void BuildSkullGeometryBuffers();
+
+protected:
+
+	ID3D11Buffer* mShapesVB;
+	ID3D11Buffer* mShapesIB;
+
+	ID3D11Buffer* mSkullVB;
+	ID3D11Buffer* mSkullIB;
+
+	ID3D11ShaderResourceView* mFloorTexSRV;
+	ID3D11ShaderResourceView* mStoneTexSRV;
+	ID3D11ShaderResourceView* mBrickTexSRV;
+
+	DirectionalLight mDirLights[3];
+	Material mGridMat;
+	Material mBoxMat;
+	Material mCylinderMat;
+	Material mSphereMat;
+
+	Material mSkullMat;
+
+	//Material mCenterSphereMat;
+
+	// Define transformations from local spaces to world space.
+	XMFLOAT4X4 mSphereWorld[10];
+	XMFLOAT4X4 mCylWorld[10];
+	XMFLOAT4X4 mBoxWorld;
+	XMFLOAT4X4 mGridWorld;
+
+	XMFLOAT4X4 mSkullWorld; // just load the skull model, but do nothing to its position
+
+	int mBoxVertexOffset;
+	int mGridVertexOffset;
+	int mSphereVertexOffset;
+	int mCylinderVertexOffset;
+
+	UINT mBoxIndexOffset;
+	UINT mGridIndexOffset;
+	UINT mSphereIndexOffset;
+	UINT mCylinderIndexOffset;
+
+	UINT mBoxIndexCount;
+	UINT mGridIndexCount;
+	UINT mSphereIndexCount;
+	UINT mCylinderIndexCount;
+
+	UINT mSkullIndexCount;
+
+	UINT mLightCount;
+
+};
